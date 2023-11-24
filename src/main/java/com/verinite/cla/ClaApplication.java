d6178a83e89f61b66339fb991490498218c31c7a
Sumeet Bandgar <sbandgar@ix.lentra.ai>
2023-11-24 16:35:05 +0530

    initial baseline commit

---
+++ src/main/java/com/verinite/cla/ClaApplication.java
@@ -0,0 +1,13 @@
+package com.verinite.cla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaApplication.class, args);
+	}
+
+}
