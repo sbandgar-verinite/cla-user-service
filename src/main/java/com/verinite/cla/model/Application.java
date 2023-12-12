153dd90ceaa5e00e8b7c091cdb2360bc75b12719
Sumeet Bandgar <sumeet.bandgar@verinite.com>
2023-12-12 15:35:59 +0530

    Added Application entities

---
+++ src/main/java/com/verinite/cla/model/Application.java
@@ -0,0 +1,89 @@
+package com.verinite.cla.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "application")
+public class Application {
+
+	@Id
+	@Column(name = "application_id")
+	@JsonProperty("application_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@JsonProperty("application_number")
+	private String applicationNumber;
+
+	@JsonProperty("application_name")
+	private String applicationName;
+
+	@JsonProperty("status")
+	private String status;
+
+	@JsonProperty("tenant_id")
+	private String tenantId;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Tenant> tenants = new ArrayList<>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getApplicationNumber() {
+		return applicationNumber;
+	}
+
+	public void setApplicationNumber(String applicationNumber) {
+		this.applicationNumber = applicationNumber;
+	}
+
+	public String getApplicationName() {
+		return applicationName;
+	}
+
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<Tenant> getTenants() {
+		return tenants;
+	}
+
+	public void setTenants(List<Tenant> tenants) {
+		this.tenants = tenants;
+	}
+
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+}
---
+++ src/main/java/com/verinite/cla/model/Tenant.java
@@ -0,0 +1,106 @@
+package com.verinite.cla.model;
+
+import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tenant")
+public class Tenant {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@JsonProperty("tenant_id")
+	private String tenantId;
+
+	@JsonProperty("tenant_name")
+	private String tenantName;
+
+	@JsonProperty("status")
+	private String status;
+
+	@JsonProperty("created_on")
+	private ZonedDateTime createdOn;
+
+	@JsonProperty("created_by")
+	private String createdBy;
+
+	@JsonProperty("modified_on")
+	private ZonedDateTime modifiedOn;
+
+	@JsonProperty("modified_by")
+	private String modifiedBy;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	public String getTenantName() {
+		return tenantName;
+	}
+
+	public void setTenantName(String tenantName) {
+		this.tenantName = tenantName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public ZonedDateTime getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(ZonedDateTime createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public ZonedDateTime getModifiedOn() {
+		return modifiedOn;
+	}
+
+	public void setModifiedOn(ZonedDateTime modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+}
