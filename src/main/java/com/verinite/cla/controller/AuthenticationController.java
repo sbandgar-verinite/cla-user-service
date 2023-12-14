dd4cccdde54a77b99c5147fe2aacbed83dab6094
Sumeet Bandgar <sumeet.bandgar@verinite.com>
2023-12-14 14:10:11 +0530

    updated endpoint

--- src/main/java/com/verinite/cla/controller/AuthenticationController.java
+++ src/main/java/com/verinite/cla/controller/AuthenticationController.java
@@ -16,7 +16,7 @@ import com.verinite.cla.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 	@Autowired
