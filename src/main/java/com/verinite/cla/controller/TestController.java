7a0a4ee43b1f83886fe71fa0d14b6a8c008848fb
Sumeet Bandgar <sumeet.bandgar@verinite.com>
2023-12-28 10:34:18 +0530

    Added configuration

--- src/main/java/com/verinite/cla/controller/TestController.java
+++ src/main/java/com/verinite/cla/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.verinite.cla.controller;
 
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +20,17 @@ public class TestController {
 		return ResponseEntity.ok("Test");
 	}
 
+	@PostMapping("/execute")
+	public String executeJs(@RequestBody String jsCode) {
+		try (Context context = Context.create()) {
+			Value result = context.eval("js", jsCode);
+
+			Object javaObject = result.as(Object.class);
+			if (javaObject != null) {
+				return "Result of JavaScript Execution: " + javaObject.toString();
+			} else {
+				return "Result of JavaScript Execution is null or undefined.";
+			}
+		}
+	}
 }
