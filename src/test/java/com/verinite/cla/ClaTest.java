6d7956b24b80a7b16dde54bcf39e82e5eefcccf6
Sumeet Bandgar <sumeet.bandgar@verinite.com>
2024-01-15 18:43:59 +0530

    Added a test case

---
+++ src/test/java/com/verinite/cla/ClaTest.java
@@ -0,0 +1,16 @@
+package com.verinite.cla;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class ClaTest {
+
+	@Test
+    public void getClaTest()  throws Exception   {
+            int initiationId = 200;
+            assertEquals(200, initiationId);
+    }
+	
+}
