28b24f2dae274a158829a809a69dd76cbf62c37a
Sumeet Bandgar <sumeet.bandgar@verinite.com>
2024-01-16 17:53:19 +0530

    Updated test case and init script for junit

--- src/main/resources/db/migration/V202312010422__CREATE_SCRIPTS.sql
+++ src/main/resources/db/migration/V202312010422__CREATE_SCRIPTS.sql
@@ -65,18 +65,6 @@ CREATE TABLE `application` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `tenant` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `created_on` datetime(6) DEFAULT NULL,
-  `modified_by` varchar(255) DEFAULT NULL,
-  `modified_on` datetime(6) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  `tenant_code` varchar(255) DEFAULT NULL,
-  `tenant_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE `application_tenant` (
   `application_id` bigint,
   `tenant_id` bigint,
--- src/test/java/com/verinite/cla/ClaTest.java
+++ src/test/java/com/verinite/cla/ClaTest.java
@@ -1,16 +1,51 @@
 package com.verinite.cla;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 public class ClaTest {
 
-	@Test
-    public void getClaTest()  throws Exception   {
-            int initiationId = 200;
-            assertEquals(200, initiationId);
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void updateConfig() throws IOException {
     }
 	
+	@Test
+	public void getClaTest() throws Exception {
+		int initiationId = 200;
+		assertEquals(200, initiationId);
+	}
+
+	@Test
+	public void getClaTestResult() throws IOException {
+		try {
+			MvcResult mvcResult = mockMvc
+					.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/application/get/all")
+							.contentType(APPLICATION_JSON_VALUE).header("tenant-id", 1))
+					.andExpect(status().isOk()).andReturn();
+			assertEquals(200, mvcResult.getResponse().getStatus());
+		} catch (Exception e) {
+			throw new RuntimeException("Cla test result : " + e);
+		}
+	}
 }
